58d9a56c2d1914dba4a48beefff997c07503b474
WillKre <7396157+WillKre@users.noreply.github.com>
2023-06-15 22:47:24 +0100

    docs: add python docstrings

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 
-# Get Secrets from AWS Secrets Manage
 def get_secrets_from_secrets_manager(secret_name):
-    secret_name = secret_name
+    """
+    Retrieves secrets from AWS Secrets Manager.
+
+    Args:
+        secret_name (str): The name of the secret to retrieve.
+
+    Returns:
+        dict: The secrets as a dictionary.
+
+    Raises:
+        ClientError: If the secrets could not be retrieved.
+    """
+
     region_name = "eu-west-1"
     service_name = 'secretsmanager'
 
@@ -22,11 +33,17 @@ def get_secrets_from_secrets_manager(secret_name):
     except ClientError as e:
         raise e
 
-    secret = get_secret_value_response['SecretString']
+    return json.loads(get_secret_value_response['SecretString'])
 
-    return json.loads(secret)
 
 def getTweepyClient():
+    """
+    Constructs and returns a Tweepy client configured with the provided credentials.
+
+    Returns:
+        tweepy.Client: A Tweepy client.
+    """
+
     return tweepy.Client(
         bearer_token=credentials['bearer_token'],
         consumer_key=credentials['consumer_key'],
@@ -35,42 +52,49 @@ def getTweepyClient():
         access_token_secret=credentials['access_token_secret']
     )
 
-# Get Recent Tweets via Tweepy
+
 def getTweets():
+    """
+    Searches and returns recent tweets mentioning the bot.
+
+    Returns:
+        tweepy.Response: A Tweepy response object containing the found tweets.
+    """
+
     client = getTweepyClient()
     bot_name = 'SummariseGPT'
     query = f"@{bot_name}"
     max_results = 10
 
-    # referenced_tweets is required to get the "parent" tweet content (either the tweet that got quoted, or replied to)
     return client.search_recent_tweets(query=query, max_results=max_results, tweet_fields=["referenced_tweets"])
 
+
 def respondToTweets(tweets):
+    """
+    Prints the content of the provided tweets and any associated parent tweets.
+
+    Args:
+        tweets (tweepy.Response): A Tweepy response object containing the tweets to respond to.
+    """
+
     client = getTweepyClient()
 
     if tweets.data is not None:
         for tweet in tweets.data:
             print(f"Tweet is: {tweet.text}")
-            print(f"Keys are: {', '.join([attr for attr in dir(tweet) if not attr.startswith('_')])}")
 
-            # Check if the tweet is a reply or a quote
+            # If the tweet references other tweets, retrieve and print their content as well
             if hasattr(tweet, 'referenced_tweets') and tweet.referenced_tweets is not None:
                 for ref_tweet in tweet.referenced_tweets:
-                    # Fetch the parent tweet data
                     parent_tweet = client.get_tweet(ref_tweet.id)
                     print(f"Parent tweet is: {parent_tweet.data.text}")
             else:
                 print(f"Unable to retrieve parent tweet for tweet id: {tweet.id}")
-
     else:
         print("No tweets found!")
 
+
 credentials = get_secrets_from_secrets_manager('summarise-gpt-prod')
 
-print(f"Credentials keys from get_secrets_from_secrets_manager: {list(credentials.keys())}")
-
 tweets = getTweets();
-    
-print(f"Tweet from getTweets: {tweets}")
-
 respondToTweets(tweets);
