1eff3aa31284c941fa3ea3e511d60af027c491dd
WillKre <7396157+WillKre@users.noreply.github.com>
2023-06-15 22:37:21 +0100

    feat: access parent tweet

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import boto3
+import tweepy
 import json
+import boto3
+from botocore.exceptions import ClientError
 
-def get_secret(secret_name):
+# Get Secrets from AWS Secrets Manage
+def get_secrets_from_secrets_manager(secret_name):
     secret_name = secret_name
     region_name = "eu-west-1"
+    service_name = 'secretsmanager'
 
     session = boto3.session.Session()
     client = session.client(
-        service_name='secretsmanager',
+        service_name=service_name,
         region_name=region_name
     )
 
@@ -19,10 +23,54 @@ def get_secret(secret_name):
         raise e
 
     secret = get_secret_value_response['SecretString']
+
     return json.loads(secret)
 
-# Get credentials from AWS Secrets Manager
-credentials = get_secret('summarise-gpt-prod')
+def getTweepyClient():
+    return tweepy.Client(
+        bearer_token=credentials['bearer_token'],
+        consumer_key=credentials['consumer_key'],
+        consumer_secret=credentials['consumer_secret'],
+        access_token=credentials['access_token'],
+        access_token_secret=credentials['access_token_secret']
+    )
 
-# Log the keys of the credentials dictionary (not the values, for security)
-print(f"Credentials keys: {list(credentials.keys())}")
+# Get Recent Tweets via Tweepy
+def getTweets():
+    client = getTweepyClient()
+    bot_name = 'SummariseGPT'
+    query = f"@{bot_name}"
+    max_results = 10
+
+    # referenced_tweets is required to get the "parent" tweet content (either the tweet that got quoted, or replied to)
+    return client.search_recent_tweets(query=query, max_results=max_results, tweet_fields=["referenced_tweets"])
+
+def respondToTweets(tweets):
+    client = getTweepyClient()
+
+    if tweets.data is not None:
+        for tweet in tweets.data:
+            print(f"Tweet is: {tweet.text}")
+            print(f"Keys are: {', '.join([attr for attr in dir(tweet) if not attr.startswith('_')])}")
+
+            # Check if the tweet is a reply or a quote
+            if hasattr(tweet, 'referenced_tweets') and tweet.referenced_tweets is not None:
+                for ref_tweet in tweet.referenced_tweets:
+                    # Fetch the parent tweet data
+                    parent_tweet = client.get_tweet(ref_tweet.id)
+                    print(f"Parent tweet is: {parent_tweet.data.text}")
+            else:
+                print(f"Unable to retrieve parent tweet for tweet id: {tweet.id}")
+
+    else:
+        print("No tweets found!")
+
+credentials = get_secrets_from_secrets_manager('summarise-gpt-prod')
+
+print(f"Credentials keys from get_secrets_from_secrets_manager: {list(credentials.keys())}")
+
+tweets = getTweets();
+    
+print(f"Tweet from getTweets: {tweets}")
+
+respondToTweets(tweets);
